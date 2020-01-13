ec1abdccbb52fc4fd840aa453b818957ec189b1f
mrarthurwhite <mister.arthur.white@gmail.com>
2020-01-13 04:42:39 -0500

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,6 +2,7 @@ require_relative "../lib/scraper.rb"
 require_relative "../lib/student.rb"
 require 'nokogiri'
 require 'colorize'
+require 'pry'
 
 class CommandLineInterface
   BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
@@ -11,8 +12,14 @@ class CommandLineInterface
     add_attributes_to_students
     display_students
   end
+=begin
+  => [{:name=>"Ryan Johnson", :location=>"New York, NY", :profile_url=>"students/ryan-johnson.html"},
+      {:name=>"Eric Chu", :location=>"Glenelg, MD", :profile_url=>"students/eric-chu.html"},
+      {:name=>"Aaron Enser", :location=>"Scottsdale, AZ", :profile_url=>"students/aaron-enser.html"},
+=end
 
   def make_students
+    # https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
     students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
     Student.create_from_collection(students_array)
   end
--- lib/student.rb
+++ lib/student.rb
@@ -4,20 +4,43 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student)
+
+    student.each do |k,v|
+      self.send("#{k}=",v)
+    end
+=begin
+        @name = student[:name]
+        @location = student[:location]
+        @profile_url = student[:profile_url]
+=end
+    @@all<< self
   end
+
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(a)
+    a.each do |k,v|
+      self.send("#{k}=",v)
+    end
+=begin
+    @twitter = a[:twitter]
+    @linkedin= a[:linkedin]
+    @github=a[:github]
+    @blog = a[:blog]
+    @profile_quote=a[:profile_quote]
+    @bio = a[:bio]
+=end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
